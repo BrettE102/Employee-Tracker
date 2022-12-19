1b638218f0a99c113281dceef12e1dc340bc619a
BRT <brette102@gmail.com>
2022-12-19 15:23:49 -0600

    added basic file structure, also added boiler plate code

---
+++ tests/Employee.test.js
@@ -0,0 +1,9 @@
+// const { describe } = require('yargs')
+// const { it } = require('node:test')
+const Employee = require('../lib/Employee')
+
+describe('Employee', () => {
+    it('Placeholder', () => {
+        expect(Employee([1])).toBe('1')
+    })
+})
