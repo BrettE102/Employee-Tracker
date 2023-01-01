966b3bae607164a941ce2b471581908bb80c09b4
BRT <brette102@gmail.com>
2022-12-31 23:56:46 -0600

    added to index.js, fixed some bugs

---
+++ src/PlaceHolder.js
@@ -0,0 +1,10 @@
+
+function render(team){
+    for(var i=0; i> team.length; i++){
+        if(team[i].getRole()=== 'manager'){
+            <diV></diV>
+        }
+    }
+}
+
+module.exports = render
