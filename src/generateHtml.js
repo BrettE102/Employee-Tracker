97234a857e2e831aa9783b99f4e65225e0e7d972
BRT <brette102@gmail.com>
2023-01-29 02:02:05 -0600

    fixed errors , added styling

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -10,11 +10,11 @@ module.exports = {
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width = device-width, initial scale = 1.0">
     <meta http-equiv = "X-UA-Compatible" content = "ie = edge">
-    <title>My Team</title>
     <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
     <link rel = "stylesheet" href = "../src/style.css">
+    <title>My Team</title>
   </head>
   <body>
     <div class = "titlebanner">
